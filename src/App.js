8fd15c0ae6854a12df2d82099b040810c9183b66
k-marek <k.brzakala@gmail.com>
2021-04-19 18:35:35 +0200

    structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  HashRouter as Router,
+  Route,
+  Link,
+  Switch,
+  NavLink,
+} from "react-router-dom";
+import * as Scroll from "react-scroll";
+import {
+  Link as LinkS,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
+
+import Home from "./components/Home"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navi from "./Navi";
+import Header from "./Header";
+
+
+const Home = () => {
+  return (
+    <>
+      <Navi />
+      <Header />
+      
+    </>
+  );
+};
+
+export default Home;
