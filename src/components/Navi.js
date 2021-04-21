96a61aff5d96007bbbb00f436fcd9bde73f94394
k-marek <k.brzakala@gmail.com>
2021-04-21 22:25:05 +0200

    routing

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -1,24 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Route,
   Link,
-  Switch,
   NavLink,
+  Switch,
 } from "react-router-dom";
-import * as Scroll from "react-scroll";
-import {
-  Link as LinkScroll,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller,
-} from "react-scroll";
-import Home from "./Home"
 import About from "./About";
+import Banner from "./Banner";
 import Exp from "./Exp";
+
 
 const Navi = () => {
   return (
@@ -27,31 +18,32 @@ const Navi = () => {
           <section className="menu">
             <div className="navi-container">
                 <nav>
-                <NavLink exact to="/">K<span>M</span></NavLink>
+                <Link to="/" className="navi-logo">K<span>M</span></Link>
                   <ul>
-                    <NavLink  to="/about">a<span>b</span>out</NavLink>
-                    <NavLink to="/experience">e<span>x</span>perience</NavLink>
+                    <Link to="/about">a<span>b</span>out</Link>
+                    <NavLink  to="/experience">e<span>x</span>perience</NavLink>
                     <NavLink  to="/portfolio">p<span>o</span>rtfolio</NavLink>
                     <NavLink  to="/contact">c<span>o</span>ntact</NavLink>
-                    <Switch>
-                      <Route  path="/about">
-                        <About />
-                      </Route>
-                      <Route  path="/experience">
-                        <Exp />
-                      </Route>
-                    </Switch>
-                  </ul>                    
+                    </ul>                    
                 </nav>
+                    
             </div>
           </section>
-          
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/experience">
+                <Exp />
+              </Route>
+              <Route  path="/">
+                <Banner />
+              </Route>
+            </Switch>
       </Router>
     </>
   );
 };
 
 export default Navi;
-
-
 
